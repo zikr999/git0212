f4d6f40d9f3c31d7a56dd47a567b917ab8b8bad6
zoumh-zz <107450585+zikr999@users.noreply.github.com>
2022-06-14 23:01:58 +0800

    pull test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello git4");
         System.out.println("hello git5");
         System.out.println("push git5");
+        System.out.println("pull git5");
     }
 }
