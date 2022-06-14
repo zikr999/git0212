19343b1fc15e3e3e4b48681d3834722b5c95e972
atguigu <atguigu@atguigu.com>
2022-06-14 22:35:40 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author yhm
+ * @create 2022-06-14 22:25
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
